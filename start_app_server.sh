e434860ff20c48e8674d1b792beaaae84398f86f
venkata.mohan <venkata.mohan@172.31.98.125>
2018-10-03 17:06:46 +0800

    Update mongo db url

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://venkat:venkat1@ds121183.mlab.com:21183/mytest
+#export MONGODB_CREDENTIALS=mongodb://venkat1:venkat2@ds121183.mlab.com:21183/mytest
 
 # finally, run the app in dev mode...
 npm run dev
